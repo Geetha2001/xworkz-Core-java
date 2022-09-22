093a06fb7657f1bf02fa6840255b0fb0984103f0
Geetha2001 <geethad.xworkz@gmail.com>
2022-09-22 11:32:47 +0530

    committing array related java files

---
+++ AdditionTester.java
@@ -0,0 +1,9 @@
+class AdditionTester{
+public static void main(String a[]){
+Addition.add(34,65);
+Addition.add(56,98,87);
+
+
+}
+
+}
---
+++ ArthmeticOperator.java
@@ -0,0 +1,35 @@
+class ArthmeticOperator
+{
+
+public static void add(int a,int b){
+System.out.println(a+b);
+}
+
+public static void add(int a,int b,int c){
+System.out.println(a+b+c);
+}
+
+public static void sub(int a,int b){
+System.out.println(a-b);
+}
+
+public static void sub(int a,int b,int c){
+System.out.println(a-b-c);
+}
+
+public static void multiply(int a,int b){
+System.out.println(a*b);
+}
+
+public static void multiply(int a,int b,int c){
+System.out.println(a*b*c);
+}
+
+public static void devide(int a,int b){
+System.out.println(a/b);
+}
+
+public static void devide(int a,int b,int c){
+System.out.println(a/b/c);
+}
+}
