8f390661b8c1a266cf7680496d4c33bca3eb0ca9
Geetha2001 <geethad.xworkz@gmail.com>
2022-09-23 13:21:37 +0530

    commting the methodoverloading related files

---
+++ SwiggyTester.java
@@ -0,0 +1,11 @@
+class SwiggyTester{
+	public static void main(String args[]){
+
+		
+
+		System.out.println(Swiggy.takeOrder("Mosaranna"));
+
+	    System.out.println(Swiggy.takeOrder("Mosaranna",69));
+      
+}
+}
